8286f88d774574497bd630e85c8c37091567ea39
Lasse Johansen <lasse.johansen@solid.no>
2020-03-22 18:42:26 +0100

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Mvc.Models
+{
+     [Table("Employees")]
+    public class Employee
+    {
+        [Column("EmployeeID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage ="Ansattnr er påkrevet")]
+        [Display(Name ="Ansatt nr")]
+        public int EmployeeID { get; set; }
+
+
+    }
+}
