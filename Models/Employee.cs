bf661b54f1346e345e5d84e9bbd4dfd3a749f3cf
Lasse Johansen <lasse.johansen@solid.no>
2020-03-24 18:02:41 +0100

    First version

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,16 +7,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManager.Mvc.Models
 {
-     [Table("Employees")]
+    [Table("Employees")]
     public class Employee
     {
         [Column("EmployeeID")]
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Required(ErrorMessage ="Ansattnr er påkrevet")]
-        [Display(Name ="Ansatt nr")]
+        [Required(ErrorMessage = "Ansattnr er påkrevet")]
+        [Display(Name = "Ansatt nr")]
         public int EmployeeID { get; set; }
 
+        [Column("FirstName")]
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Fornavn er påkrevet")]
+        [StringLength(10, ErrorMessage = "Fornavn må være kortere enn 10 tegn")]
+        public string FirstName { get; set; }
 
+        [Column("LastName")]
+        [Display(Name = "Etternavn")]
+        [Required(ErrorMessage = "Navn er påkrevet")]
+        [StringLength(10, ErrorMessage = "Etternavn må være kortere enn 20 tegn")]
+        public string LastName { get; set; }
+
+        [Column("Title")]
+        [Display(Name = "Tittel")]
+        [Required(ErrorMessage = "Tittel er påkrevet")]
+        [StringLength(30, ErrorMessage = "Maks 30 tegn i tittel")]
+        public string Title { get; set; }
+
+        [Column("BirthDate")]
+        [Display(Name = "Fødselsdato")]
+        [Required(ErrorMessage = "Du må angi fødselsdato")]
+        public DateTime BirthDate { get; set; }
+        
+        [Column("HireDate")]
+        [Display(Name ="Ansatt dato")]
+        [Required(ErrorMessage ="Du må angi ansatt dato")]
+        public DateTime HireDate { get; set; }
+
+        [Column("Country")]
+        [Display(Name ="Land")]
+        [Required(ErrorMessage ="Du må angi land")]
+        [StringLength(15,ErrorMessage ="Maks 15 tegn")]
+        public string Country { get; set; }
+
+        [Column("Notes")]
+        [Display(Name ="Notater")]
+        [StringLength(500, ErrorMessage ="Maks 500 tegn")]
+        public string Notes { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManager.Mvc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +15,20 @@ namespace EmployeeManager.Mvc
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(this.config.GetConnectionString("AppDb")));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,15 +38,18 @@ namespace EmployeeManager.Mvc
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
             });
         }
+
+        private IConfiguration config = null;
+
     }
 }
